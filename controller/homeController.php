f5ad47ce3c137297fb677bff88a8794118add931
jrdemain <jrdemain@gmail.com>
2016-04-26 18:33:14 -0700

    Added contact page

--- controller/homeController.php
+++ controller/homeController.php
@@ -9,10 +9,22 @@ include( APP_VIEW . '/nav.php' );
 
 switch ( $route->getAction() ) {
 
+    case 'contact':
+        if (isset($_POST['submit'])) {
+          print '<pre>';
+          print_r($_POST);
+          print '</pre>';
+      } else {
+        include( APP_VIEW .'/home/homeSubNav.php' );
+        include( APP_VIEW .'/home/contactView.php' );
+      }
+      break;
+
     case 'home':
         include( APP_VIEW .'/home/homeSubNav.php' );
         include( APP_VIEW .'/home/homeView.php' );
-        break;
+    break;
+
 
     default:
         include( APP_VIEW .'/home/homeSubNav.php' );
